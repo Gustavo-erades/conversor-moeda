1d05c7f36537c9fda36f34ebb4768c71936cb8d8
Gustavo Êrades Vilarinho Silva <108373134+Gustavo-erades@users.noreply.github.com>
2023-03-29 16:19:11 -0300

    Add files via upload

---
+++ index-casoErro.php
@@ -0,0 +1,73 @@
+<?php
+    //consumir API do Banco Central para pegar o valor de câmbio das moedas em tempo real
+    //pegar a data
+    $inicio=date("m-d-Y",strtotime("-7 days"));
+    $fim=date("m-d-Y");
+    //dólar
+    $url_dolar='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+    $dados_dolar=json_decode(file_get_contents($url_dolar), true);
+    $cotacao_dolar=$dados_dolar["values"][0]["cotacaoCompra"];
+    //euro
+    $url_euro='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda=\'EUR\'&@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+    $dados_euro=json_decode(file_get_contents($url_dolar),true);
+    $cotacao_euro=$dados_euro["values"][0]["cotacaoCompra"];
+    //libra
+    $url_libra='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda=\'GBP\'&@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+    $dados_libra=json_decode(file_get_contents($url_libra), true);
+    $cotacao_libra=$dados_libra["values"][0]["cotacaoCompra"];
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="icone-moeda.png" type="image/x-icon">
+    <title>Conversor de moeda</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="container">
+        <div id="formulario">
+            <form action="resultado.php" method="post">
+                <div id="valor_user">
+                    <h1>Valor a converter:</h1>
+                    <span>R$</span>
+                    <input type="number" name="num" step="0.01">
+                </div>
+                <div id="moedas">
+                    <h2>Moedas:</h2>
+                    <table class="table table-striped table-dark">
+                        <thead>
+                            <tr>
+                                <th scope="col"></th>
+                                <th scope="col">Moeda</th>
+                                <th scope="col">Cotação Atual</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row"><input type="radio" name="moeda" id="dolar" value="dolar"></th>
+                                <td><label for="dolar">Dólar</label></td>
+                                <td><label for="dolar" target="_blank" data-toggle="tooltip" data-placement="right" title="currículo Lattes"><?php print $cotacao_dolar?></label></td>
+                            </tr>
+                            <tr>
+                                <th scope="row"><input type="radio" name="moeda" id="euro" value="euro"></th>
+                                <td><label for="euro">Euro</label></td>
+                                <td><label for="euro"><?php print $cotacao_euro ?></label></td>
+                            </tr>
+                            <tr>
+                                <th scope="row"><input type="radio" name="moeda"  id="libra" value="libra"></th>
+                                <td><label for="libra">Libra</label></td>
+                                <td><label for="libra"><?php print $cotacao_libra ?></label></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <button type="submit">converter</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
